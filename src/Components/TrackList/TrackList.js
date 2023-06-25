f77d28cba221cb7dfc5566c54505e6b12e3b2cbe
Shayan-Farhan <safarhan025@gmail.com>
2023-06-25 13:18:02 -0400

    TrackList component (rendering tracks)

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./TrackList.css";
+import Track from "../Track/Track";
+
+function TrackList (props) {
+    return (
+        <section className = "TrackList">
+            {props.tracks.map(
+                (track) => {
+                    return (
+                        <Track
+                          track={track}
+                          key={track.id}
+                          onAdd={props.onAdd}
+                          isRemoval = {props.isRemoval}
+                          onRemove = {props.onRemove}
+                          />
+                    );
+                }
+            )};
+        </section>
+    );
+;}
+
+export default TrackList;
