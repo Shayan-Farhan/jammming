d23056c6d657b60a4435b253d22e88f5bf93ea2b
Shayan-Farhan <safarhan025@gmail.com>
2023-06-26 18:10:52 -0400

    Built SearchBar component

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState, useCallback } from "react";
+import "./SearchBar.css";
+
+function SearchBar (props) {
+    const [song, setSong] = useState('');
+
+    const handleNewSearch = useCallback (
+        (event) => {
+            setSong(event.target.value);
+        }, []
+    );
+
+    const search = useCallback (
+        (event) => {
+            props.onSearch(song);
+        }, [props.onSearch, song]
+    );
+
+    return (
+        <section className = 'Search'>
+            <input className="SearchBar" placeholder="Enter a Song" onChange = {handleNewSearch} />
+            <button className = "SearchButton" onClick = {search}>
+                Search
+            </button>
+        </section>
+    );
+}
+
+export default SearchBar;
