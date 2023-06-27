5f9baff6a9d153bb04b698c45f3323242d6f96f5
Shayan-Farhan <125101939+Shayan-Farhan@users.noreply.github.com>
2023-06-27 18:31:28 -0500

    Started App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,1 +1,12 @@
+import React, { useState, useCallback } from "react";
+import "./App.css";
+import Playlist from "../Playlist/Playlist.js";
+import SearchBar from "../.SearchBar/SearchBar.js";
+import Track from "../Track/Track.js";
+import TrackList from "../TrackList/TrackList.js";
 
+function App (props) {
+  return (
+  
+   ); 
+}
