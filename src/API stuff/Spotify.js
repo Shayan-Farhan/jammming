64e97e3fc8736d96f0a9beeb387da295fd964013
Shayan-Farhan <safarhan025@gmail.com>
2023-06-28 21:41:37 -0400

    Built index.js (bottom level component)

--- src/API stuff/Spotify.js
+++ src/API stuff/Spotify.js
@@ -3,4 +3,5 @@
  * make sure that the track has a .url property
  * Make sure the function that save a playlist to spotify is called
  * "savePlaylist" and has two parameters: playlistName and trackUrls. 
+ * Remember to do App.css --- this wasn't completed with App.js!
  */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from "react"
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./Components/App/App";
+import "./index.css";
 
+const element = document.getElementById('root');
+const root = ReactDOM.createRoot(element);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
